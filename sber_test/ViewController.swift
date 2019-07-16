36943477af5b5552bc080db8ee0dd5a5e58a86fc
Denis Semenikhin <semenihind@mail.ru>
2019-07-16 16:08:31 +0300

    Initial Commit

---
+++ sber_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sber_test
+//
+//  Created by Denis on 16/07/2019.
+//  Copyright © 2019 hostis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
